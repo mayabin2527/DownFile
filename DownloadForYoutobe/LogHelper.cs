6d71aabeea7e81360fee887c48cd78c34ce4c36d
“mayabin2527” <846209981@qq.com>
2022-04-04 15:51:30 +0800

    新增请求YouTube接口类

---
+++ DownloadForYoutobe/LogHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Serilog;
+namespace DownloadForYoutobe
+{
+    public class LogHelper
+    {
+        public LogHelper()
+        {
+        }
+
+        public static void INFO(string msg) {
+
+            try
+            {
+
+            Log.Logger = new LoggerConfiguration()
+               .WriteTo.File("logs/myapp.txt")
+               .CreateLogger();
+
+            Log.Information(msg);
+
+
+            Log.CloseAndFlush();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
--- DownloadForYoutobe/Program.cs
+++ DownloadForYoutobe/Program.cs
@@ -6,8 +6,10 @@ namespace DownloadForYoutobe
     {
         public static void Main(string[] args)
         {
-            new RestSharpHelper().getVideos("1");
-            //1 
+            //new RestSharpHelper().getVideos("1");
+            //1
+            LogHelper.INFO("Start");
+           Google.Apis.YouTube.Samples.Search.Main1(null);
             Console.WriteLine("Hello World!");
         }
     }
